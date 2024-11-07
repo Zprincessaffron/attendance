4c82e40aafaec6e5364339d18843a317c18249f2
PREMKUMAR5519 <premkumarananthan1@gmail.com>
2024-11-07 16:03:07 +0530

    setted on loading

--- client/src/context/ProtectedRoute.jsx
+++ client/src/context/ProtectedRoute.jsx
@@ -1,23 +1,29 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { EmployeeContext } from './EmployeeContext';
+import { jwtDecode } from "jwt-decode";
 
 const ProtectedRoute = ({ allowedRoles }) => {
   const { employeeData } =  useContext(EmployeeContext)
-  const [userRole,setUserRole]=useState(employeeData.accessCode)
+  console.log(employeeData)
   
+  useEffect(() => {
+    try {
+        const token =  localStorage.getItem('token');
+        const decodedToken = jwtDecode(token);
+        // Extract relevant information from the token
+        const { employeeId, role, department, name } = decodedToken;
+        setUserRole(decodedToken.accessCode)
+    } catch (error) {
+        
+    }
+  }, [ ])
+  const [userRole,setUserRole]=useState()
 
-  // function funGetAccess(){
-  //   setUserRole(employeeData.accessCode)
-  // }
-  // useEffect(() => {
-  //   funGetAccess()
-  // }, [])
-  
   
 
   // If userRole is in allowedRoles, allow access; otherwise, redirect to login
-  return allowedRoles.includes(userRole) ? <Outlet /> : <Navigate to="/" />;
+  return allowedRoles.includes(userRole) ? <Outlet /> : <div>hello</div>;
 };
 
 export default ProtectedRoute;
